9735c9da8fb4ea4ff73f5d5dc76fe487c78bab52
vvcb <vvcb.n1@gmail.com>
2023-12-10 00:22:25 +0000

    Working mostly!
    - Fixed CORS issues. The init container in the OPAL deployment will copy the data in opal-configmap to the correct location.
    - Ingress is setup and appears to route correctly, I have created a domain name for $12 to allow CORS!!!
    - Storage for OPAL is Azure file storage again
    
    - Authentication was getting stuck with a progress bar. However, on the opal pod logs, it is clear that authenitication has been successful and a session is created for the user, But nothing happens at the client end after this. The reason for this was that this requires a  secure cookie to be set at the client that cannot be set unless the connection is secured over TLS. I have now added TLS support with letsencrypt providing the SSL certificates  - and viola it works.
    
    - We can now start looking at the rest of the Obiba stack and we are good to go. It will be nice if we can get all this from our European colleages.

--- charts/nginx_install.sh
+++ charts/nginx_install.sh
@@ -1,4 +1,4 @@
-# helm repo add chgl https://chgl.github.io/charts
+# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 # helm repo update
 #====================================================================
 cp -r /mnt/c/Users/vishnu.chandrabalan/.kube/config ~/.kube/config
@@ -9,12 +9,11 @@ AKSNAME=aks-datashield-dev-01
 kubectl config use-context $AKSNAME
 #====================================================================
 # need a mechanism to change this between prd and dev
-NS=obiba
-HELM_RELEASE_NAME=obiba01
-VERSION=0.0.1
-
-helm uninstall $HELM_RELEASE_NAME \
-    --namespace $NS \
+NS=ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --create-namespace \
+  --namespace $NS \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
 
 #====================================================================
 kubectl config use-context $CURRENTAKSCONTEXT
