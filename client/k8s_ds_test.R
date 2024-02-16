b5a9ee403f5c74331adae39d7961643b6efd81e9
OllyButters <oliver.butters@gmail.com>
2024-02-16 14:59:27 +0000

    demo

--- client/k8s_ds_test.R
+++ client/k8s_ds_test.R
@@ -12,7 +12,7 @@ library(opalr)
 # Use the opal admin package to get server level info
 
 # Set all the options.
-options(opal.url = "https://datashield1.liv.ac.uk")
+options(opal.url = "https://a-datashield.liv.ac.uk")
 # options(opal.url = "https://landertre-dev.org")
 
 options(opal.username = "administrator",
@@ -38,21 +38,28 @@ opal.datasources(o)
 # Connect to server
 builder <- DSI::newDSLoginBuilder()
 builder$append(server = "server1",
-               url = "https://datashield1.liv.ac.uk",
+               url = "https://a-datashield.liv.ac.uk",
                user = "administrator",
                password = "P@55w0rd",
+               table = "DEMO.CNSIM1",
+               options = "list(ssl_verifyhost = 0, ssl_verifypeer = 0)")
+builder$append(server = "server2",
+               url = "https://b-datashield.liv.ac.uk",
+               user = "administrator",
+               password = "P@55w0rd",
+               table = "DEMO.CNSIM2",
                options = "list(ssl_verifyhost = 0, ssl_verifypeer = 0)")
 logindata <- builder$build()
-connections <- datashield.login(logins = logindata, assign = TRUE)
+connections <- datashield.login(logins = logindata, assign = TRUE, symbol = "D")
 
 # Check DataSHIELD
 datashield.pkg_status(connections)
 datashield.tables(connections)
 
-# Assign the table "test.CNSIM1" to the symbol 'D' Note that the table here
+# Assign the table "DEMO.CNSIM1" to the symbol 'D' Note that the table here
 # needs to have been uploaded to opal and it probably needs to be assumed
 # as decimals in the ingest process.
-datashield.assign.table(connections, 'D', "test.CNSIM4")
+#datashield.assign.table(connections, 'D', "DEMO.CNSIM1")
 
 # Now start doing stuff with the data in the table linked to 'D'
 ds.colnames(x = 'D', datasources = connections)
@@ -60,3 +67,44 @@ ds.dim(x = 'D', datasources = connections)
 ds.summary(x = 'D$LAB_HDL', datasources = connections)
 ds.mean(x = 'D$LAB_HDL', datasources = connections)
 datashield.errors()
+
+
+########################### Synthea
+builder <- DSI::newDSLoginBuilder()
+builder$append(server = "server1",
+               url = "https://a-datashield.liv.ac.uk",
+               user = "administrator",
+               password = "P@55w0rd",
+               table = "synthea.conditions",
+               options = "list(ssl_verifyhost = 0, ssl_verifypeer = 0)")
+
+logindata <- builder$build()
+connections <- datashield.login(logins = logindata, assign = TRUE, symbol = "D")
+
+# Check DataSHIELD
+datashield.pkg_status(connections)
+datashield.tables(connections)
+
+# Assign the table "DEMO.CNSIM1" to the symbol 'D' Note that the table here
+# needs to have been uploaded to opal and it probably needs to be assumed
+# as decimals in the ingest process.
+#datashield.assign.table(connections, 'D', "DEMO.CNSIM1")
+
+# Now start doing stuff with the data in the table linked to 'D'
+ds.colnames(x = 'D', datasources = connections)
+ds.dim(x = 'D', datasources = connections)
+
+ds.dataFrameSubset(df.name = "D",
+                   V1.name = "D$CODE",
+                   V2.name = "72892002",
+                   Boolean.operator = "==",
+                   #keep.cols = c(1:4,10), #only columns 1, 2, 3, 4 and 10 are selected
+                   rm.cols = NULL,
+                   keep.NAs = FALSE,
+                   newobj = "subset.all.rows",
+                   datasources = connections, #all servers are used
+                   notify.of.progress = FALSE)                
+
+ds.colnames("subset.all.rows")
+ds.dim("subset.all.rows")
+ds.length("subset.all.rows")
